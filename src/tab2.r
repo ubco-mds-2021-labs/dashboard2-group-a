fd1874ac367db271a8b9676bcd25d847e0d1f54d
Kristy Phipps <phipps.kristy@gmail.com>
2022-03-14 19:00:05 -0700

    added date picker to callbacks for both plots

--- src/tab2.r
+++ src/tab2.r
@@ -4,43 +4,11 @@ library(dashBootstrapComponents)
 library(ggplot2)
 library(plotly)
 library(tidyverse)
-library(rlang)
 
 #data import section 
 energy <- read_csv("https://raw.githubusercontent.com/ubco-mds-2021-labs/dashboard1-group-a/main/data/energy_df_full.csv")
 
-#functions section
-
-start_date <- "2016-01-25"
-end_date <- "2016-01-30"
-
-energy_plot <- function(){
-    p1 <- energy %>%
-    filter(date >= start_date & date <= end_date) %>%
-    pivot_longer("energy_appliances":"energy_lights", names_to ="source", values_to ="energy_usage") %>% 
-    ggplot()+
-        aes(
-        x = date,
-        y = energy_usage,
-        fill = source
-        )+
-        geom_area(stat="identity")
-    p1
-}
-
-weather_plot <-function(){
-    p2 <- energy %>%
-    filter(date >= start_date & date <= end_date) %>%
-    ggplot()+
-        aes(
-        x = date,
-        y = temperature_outside
-        )+
-        geom_line()
-    p2
-}
-
-#Helper Function
+#Helper Functions
 weather_list <-list(
     "temperature_outside",
     "dewpoint",
@@ -58,10 +26,14 @@ choice2 <- lapply(
   }
 )
 
-# #plotting section 
-
-p1 <- energy_plot()
-# p2 <- weather_plot() 
+date_picker = dccDatePickerRange(
+  id="my-date-picker-range",
+  min_date_allowed=as.Date(c("2016-01-11")),
+  max_date_allowed=as.Date(c("2016-05-27")),
+  initial_visible_month=as.Date("2016-01-11"),
+  start_date=as.Date(c("2016-01-12")),
+  end_date=as.Date(c("2016-01-13"))
+)
 
 # Create a Dash app
 app <- dash_app()
@@ -69,19 +41,55 @@ app <- dash_app()
 # Set the layout of the app
 app %>% set_layout(
   h1('Tab 2: Energy Use!!'),
-  div("Tab2: Plot1"), 
-  dccGraph(figure=ggplotly(p1)), 
+  div("Tab2: Plot1"),
+  dbcLabel("Choose Date Range:", style=list('font-size'='20px')),
+  br(),
+  dbcRow(date_picker),
+  br(), 
+  dccGraph(id="energy_plot"),
+  br(), 
   div("Tab2: Plot2"),
+  br(),
   dccDropdown(id="weather_yaxis", options=choice2, value="temperature_outside"),
+  br(),
   dccGraph(id="weather_plot") 
 )
 
 #Callback functions
+
+##Callback for plot1
 app %>% add_callback(
-    output("weather_plot", "figure"), 
-    input("weather_yaxis", 'value'), 
-function(y){
-    p <- energy %>%
+    output("energy_plot", "figure"),
+    list( 
+    input("my-date-picker-range", 'start_date'),
+    input("my-date-picker-range", 'end_date')
+    ), 
+function(start_date, end_date){
+    p1 <- energy %>%
+    filter(date >= start_date & date <= end_date) %>%
+    pivot_longer("energy_appliances":"energy_lights", names_to ="source", values_to ="energy_usage") %>% 
+    ggplot()+
+        aes(
+        x = date,
+        y = energy_usage,
+        fill = source
+        )+
+        geom_area(stat="identity")
+    
+    ggplotly(p1)
+    }
+)
+
+#Callback for plot 2
+app %>% add_callback(
+    output("weather_plot", "figure"),
+    list( 
+    input("my-date-picker-range", 'start_date'),
+    input("my-date-picker-range", 'end_date'),
+    input("weather_yaxis", 'value')
+    ), 
+function(start_date, end_date, y){
+    p2 <- energy %>%
     filter(date >= start_date & date <= end_date) %>%
     ggplot()+
         aes(
@@ -90,7 +98,7 @@ function(y){
         ) +
         geom_line()
     
-    ggplotly(p)
+    ggplotly(p2)
     }
     
 )
